76c1612bdc536e4130551579d4b8a2cf2be6e8cc
Pie315 <devonkaelberer@gmail.com>
2023-03-04 13:25:05 -0500

    worked on modal show/hide

--- javascript.js
+++ javascript.js
@@ -1,6 +1,20 @@
 const modal = document.querySelector(".modal");
+const addBook = document.querySelector(".addBook")
+const modalBox = document.querySelector(".modalBox")
+
+function showModal() {
+    modal.classList.toggle("hide")
+}
+
+modalBox.addEventListener("click", () => {
+    showModal()
+})
 
 modal.addEventListener("click", () => {
-    modal.classList.toggle("hide");
-    console.log("Hello World!");
+    showModal()
 })
+
+addBook.addEventListener("click", () => {
+    showModal()
+})
+
