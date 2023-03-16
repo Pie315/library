9ecb46b7baac1f2732be84efebc66c4e04b270d5
Pie315 <devonkaelberer@gmail.com>
2023-03-16 10:45:30 -0400

    fixed modal issues

--- javascript.js
+++ javascript.js
@@ -1,20 +1,26 @@
 const modal = document.querySelector(".modal");
-const addBook = document.querySelector(".addBook")
-const modalBox = document.querySelector(".modalBox")
+const addBook = document.querySelector("#addBook");
+const modalBox = document.querySelector(".modalBox");
+const submit = document.querySelector("#submit");
 
 function showModal() {
     modal.classList.toggle("hide")
 }
 
-modalBox.addEventListener("click", () => {
-    showModal()
-})
+
+
+modalBox.addEventListener("click", (e) => {
+    e.stopPropagation();
+});
 
 modal.addEventListener("click", () => {
     showModal()
-})
+});
 
 addBook.addEventListener("click", () => {
     showModal()
-})
+});
 
+submit.addEventListener("click", () => {
+    Event.preventDefault();
+});
